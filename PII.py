e90ac6f4790ffeeca315feac3d024e1cd338a415
AnishPalabatla <122909146+AnishPalabatla@users.noreply.github.com>
2023-08-22 20:26:38 +0530

    Update PII.py

--- PII.py
+++ PII.py
@@ -7,7 +7,7 @@ def remove_pii(text):
         if not cont:
             result.append(word)
         else:
-            result.append("[REDACTED]")
+            result.append("[CENSORED]")
     output = ' '.join(result)
     return output
 input = "Anish's email is test@temp.com and his phone number is 123-456-7890, SSN is 765-433-123"
